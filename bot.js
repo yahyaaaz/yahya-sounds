7914a32d8b7e483911f6122705d70d8370132be5
yahyaaaz <46232918+yahyaaaz@users.noreply.github.com>
2019-01-19 05:12:36 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -351,11 +351,11 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers`);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`yplay | Y A H Y A #9952`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
 client.login(process.env.BOT_TOKEN);
 client.on('ready',async () => {
-  client.channels.find(ch => ch.id === "536167560048017408" && ch.type === 'voice').join();
+  client.channels.find(ch => ch.id === "536167560048017408" && ch.type === 'voice').join(536167560048017408);
 });
